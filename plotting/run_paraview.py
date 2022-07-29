4b433ab88e70a449a3d18479fd44abc13423cad0
Matthias Frey <mf248@st-andrews.ac.uk>
2022-07-29 13:56:04 +0100

    update run paraview script

--- plotting/run_paraview.py
+++ plotting/run_paraview.py
@@ -1,14 +1,62 @@
 from iso_surface import iso_surface
+import argparse
 
-iso = iso_surface() #create_cmap=True)
+parser = argparse.ArgumentParser(description='Create iso-surface figures.')
+parser.add_argument('--filename',
+                    type=str,
+                    help='output file')
 
-iso.open('../examples/beltrami_32_fields.nc')
+parser.add_argument('--save_path',
+                    type=str,
+                    help='where to save the figures',
+                    default=os.getcwd())
 
-#iso.render(step=60, niso=10)
-#iso.export(file_path='./', file_name='test.png')
+parser.add_argument('--steps',
+                    type=int,
+                    nargs='+',
+                    help='list of steps',
+                    default=[0, 1])
 
-iso.save_camera_orbiting_animation(step=60, n_frames=360, file_path='./',
-                                   file_name="test.mp4", keep_frames=False)
+parser.add_argument('--overwrite',
+                    help='overwrite figures',
+                    action='store_true')
+
+parser.add_argument('--fignums',
+                    type=int,
+                    nargs='+',
+                    help='figure numbers',
+                    fignums=[100, 101])
+
+args = parser.parse_args()
+fname = args.filename
+steps = args.steps
+save_path = args.save_path
+overwrite = args.overwrite
+fignum = args.fignums
+
+print()
+print("\tFilename:    ", fname)
+print("\tSteps:       ", steps)
+print("\tSave path:   ", save_path)
+print("\tOverwrite:   ", overwrite)
+print("\tFignums:     ", fignums)
+print()
+
+if not len(fignums) == len(steps):
+    print("Error: The number of figures and the number of steps not identical.")
+
+
+for i, step in enumerate(steps):
+    iso = iso_surface()
+
+    iso.open(fname)
+    iso.render(step=60, niso=10)
+    iso.export(file_path=save_path, file_name='fig' + str(fignums[i]) + '.eps')
+    iso.close()
+    iso = None
+
+#iso.save_camera_orbiting_animation(step=60, n_frames=360, file_path='./',
+                                   #file_name="test.mp4", keep_frames=False)
 
 
 #iso.save_animation(beg=0, end=100,
@@ -17,4 +65,3 @@ iso.save_camera_orbiting_animation(step=60, n_frames=360, file_path='./',
                    #file_name="animation.mp4",
                    #keep_frames=True)
 
-iso.close()
