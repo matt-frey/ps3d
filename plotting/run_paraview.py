6138144913825571ca4a15b355fbfb922eff0858
Matthias Frey <mf248@st-andrews.ac.uk>
2022-07-29 13:58:13 +0100

    add n_iso argument

--- plotting/run_paraview.py
+++ plotting/run_paraview.py
@@ -17,6 +17,12 @@ parser.add_argument('--steps',
                     help='list of steps',
                     default=[0, 1])
 
+parser.add_argument('--n_iso',
+                    type=int,
+                    nargs=1,
+                    help='number of iso-surfaces',
+                    default=20)
+
 parser.add_argument('--overwrite',
                     help='overwrite figures',
                     action='store_true')
@@ -30,16 +36,18 @@ parser.add_argument('--fignums',
 args = parser.parse_args()
 fname = args.filename
 steps = args.steps
+n_iso = args.n_iso
 save_path = args.save_path
 overwrite = args.overwrite
 fignum = args.fignums
 
 print()
-print("\tFilename:    ", fname)
-print("\tSteps:       ", steps)
-print("\tSave path:   ", save_path)
-print("\tOverwrite:   ", overwrite)
-print("\tFignums:     ", fignums)
+print("\tFilename:               ", fname)
+print("\tSteps:                  ", steps)
+print("\tNumber of iso-surfaces: ", n_iso
+print("\tSave path:              ", save_path)
+print("\tOverwrite:              ", overwrite)
+print("\tFignums:                ", fignums)
 print()
 
 if not len(fignums) == len(steps):
@@ -50,7 +58,7 @@ for i, step in enumerate(steps):
     iso = iso_surface()
 
     iso.open(fname)
-    iso.render(step=60, niso=10)
+    iso.render(step=60, niso=n_iso)
     iso.export(file_path=save_path, file_name='fig' + str(fignums[i]) + '.eps')
     iso.close()
     iso = None
