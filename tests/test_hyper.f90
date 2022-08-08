4cc064038ed0155604e96266f894c3125faa61c8
Matthias Frey <mf248@st-andrews.ac.uk>
2022-08-08 16:37:27 +0100

    add test_hyper.f90

--- tests/test_hyper.f90
+++ tests/test_hyper.f90
@@ -1,10 +1,11 @@
 !==============================================================================
-  ! Tests effect of either the 2/3 de-aliasing rule or the Hou & Li filter
-  ! on an smooth field q(z) specified below.
-
-  ! We also examine the effect of the filter on dq/dz.
+! Tests effect of hyperdiffusion on an initial field q(z,0) = q_0(z)
+! in [-pi/2,pi/2],  Here, q_t = -nu (-d^2/dz^2)^p(q). We use the exact
+! solution in spectral space q_m(0) * exp(-nu * m^{2p} * t).
+!
+! We also record dq/dz.
 !==============================================================================
-program test_filter
+program test_hyper
     use stafft, only : initfft, dst, dct
     use deriv1d, only : init_deriv
     use constants, only : zero, one, two, pi
@@ -22,8 +23,9 @@ program test_filter
     double precision,parameter:: zmin = -Lz/two, zmax = zmin + Lz
 
     ! Arrays:
-    double precision:: z(0:nz), q(0:nz), qs(0:nz), filt(0:nz)
+    double precision:: z(0:nz), q(0:nz), qs(0:nz), diss(0:nz)
     double precision:: dq(0:nz), dqs(0:nz)
+    double precision:: q0(0:nz), dq0(0:nz)
 
     ! FFT arrays:
     integer:: zfactors(5)
@@ -31,7 +33,7 @@ program test_filter
 
     ! Others:
     double precision:: eps, t, fac
-    integer:: iz, kz, iopt
+    integer:: iz, nnu
 
     !-------------------------------------------------------------
     ! Initialise Fourier transform and wavenumbers:
@@ -48,41 +50,33 @@ program test_filter
     write(*,*) ' Enter the amplitude of the disturbance:'
     read(*,*) eps
     q = (z - zmin) * (Lz**2 - (z - zmin)**2) * (one + eps*q(iz))
+    q0 = q
 
-    ! Get derivative dq/dz spectrally:
-    dq = q
-    call dst(1, nz, dq(1:nz), ztrig, zfactors)
-    dq(0) = zero
-    dq(1:nz-1) = rkz(1:nz-1)*dq(1:nz-1)
-    dq(nz) = zero
-    call dct(1, nz, dq, ztrig, zfactors)
+    write(*,*) ' Hyperviscosity order (use 1 for molecular viscosity)?'
+    read(*,*) nnu
 
-    write(*,*) ' Choose (1) 2/3 rule or (2) Hou & Li filter:'
-    read(*,*) iopt
+    ! Get derivative dq0/dz spectrally:
+    qs = q
+    call dst(1, nz, qs(1:nz), ztrig, zfactors)
+    dqs(0) = zero
+    dqs(1:nz-1) = rkz(1:nz-1)*qs(1:nz-1)
+    dqs(nz) = zero
+    call dct(1, nz, dqs, ztrig, zfactors)
+    dq0 = dqs
 
-    if (iopt == 1) then
-        fac = (2.d0/3.d0)*rkz(nz)
-        do kz = 0, nz
-            if (rkz(kz) < fac) then
-                filt(kz) = one
-            else
-                filt(kz) = zero
-            endif
-        enddo
-    else
-        fac = one/rkz(nz)
-        filt(0) = one
-        do kz = 1, nz
-            filt(kz) = exp(-36.d0*(fac*rkz(kz))**36.d0)
-        enddo
-    endif
+    write(*,*) ' We take nu = 1/kz_max^{2p}.  Enter t:'
+    read(*,*) t
+    fac = one/rkz(nz)
+    diss(0) = zero
+    diss(1:nz-1) = exp(-t*(fac*rkz(1:nz-1))**(2*nnu))
+    diss(nz) = zero
 
     ! Transform to spectral space:
     qs = q
     call dst(1, nz, qs(1:nz), ztrig, zfactors)
 
-    ! Apply filter:
-    qs = filt*qs
+    ! Apply damping:
+    qs = diss*qs
 
     ! Differentiate:
     dqs(0) = zero
@@ -92,16 +86,18 @@ program test_filter
     ! Transform back to physical space:
     call dst(1, nz, qs(1:nz), ztrig, zfactors)
     call dct(1, nz, dqs, ztrig, zfactors)
+    q = qs
+    dq = dqs
 
-    open(80, file = 'q_profile_filter.asc', status = 'replace')
+    open(80, file = 'q_profile_hyper.asc', status = 'replace')
     do iz = 0, nz
-        write(80,*) z(iz), qs(iz), q(iz), qs(iz) - q(iz)
+        write(80,*) z(iz), q(iz), q0(iz), q(iz) - q0(iz)
     enddo
     close(80)
 
-    open(80, file = 'dq_profile_filter.asc', status = 'replace')
+    open(80, file = 'dq_profile_hyper.asc', status = 'replace')
     do iz = 0, nz
-        write(80,*) z(iz), dqs(iz), dq(iz), dqs(iz) - dq(iz)
+        write(80,*) z(iz), dq(iz), dq0(iz), dq(iz) - dq0(iz)
     enddo
     close(80)
-end program test_filter
+end program test_hyper
