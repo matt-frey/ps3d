b9b2e7e7c751b158d6f6a2026d26b451a26aeb80
Matthias Frey <mf248@st-andrews.ac.uk>
2022-08-10 19:57:02 +0100

    fix ordering in create_figures.py

--- plotting/create_figures.py
+++ plotting/create_figures.py
@@ -88,6 +88,14 @@ if 5 in figures:
 
 # Figure 6:
 if 6 in figures:
+    os.system("python " + os.path.join(spath, "plot_diff_velocity_rms_evolution.py") + \
+              " --filename " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
+              " --fignum 6" + \
+              " --overwrite" + \
+              " --save_path " + save_path)
+
+# Figure 7:
+if 7 in figures:
     #>>> t, ke, en = np.loadtxt('paper_runs/beltrami_256_restart_ecomp.asc', unpack=True)
     #>>> idx = np.argmax(en)
     #>>> t[idx]
@@ -105,7 +113,7 @@ if 6 in figures:
                   " --vmin 0.0" + \
                   " --opacity_vmax 1.0" + \
                   " --opacity_vmin 0.0" + \
-                  " --fignum 6" + \
+                  " --fignum 7" + \
                   " --overwrite" + \
                   " --save_path " + save_path)
 
@@ -118,10 +126,10 @@ if 6 in figures:
               " --overwrite" + \
               " --save_path " + save_path)
 
-# Figure 7:
+# Figure 8:
 # create xy-plane cross-section of vorticty magnitude
 # at iy = 128 (i.e. y = 0):
-if 7 in figures:
+if 8 in figures:
     os.system("python " + os.path.join(spath, "plot_slices_2x3.py") + \
               " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
               " --plane 'xz'" + \
@@ -129,22 +137,6 @@ if 7 in figures:
               " --zlabel '$y = 0$'" + \
               " --steps 0 10 16 20 26 40" + \
               " --file_numbers 0 0 0 0 0 0 " + \
-              " --field vorticity_magnitude" + \
-              " --colormaps rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r" + \
-              " --norms none none none log log log" + \
-              " --fignum 7" + \
-              " --overwrite" + \
-              " --save_path " + save_path)
-
-# Figure 8:
-if 8 in figures:
-    os.system("python " + os.path.join(spath, "plot_slices_2x3.py") + \
-              " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
-              " --plane 'xy'" + \
-              " --loc 128" + \
-              " --zlabel '$z = 0$'" + \
-              " --steps 0 10 16 20 26 40" + \
-              " --file_numbers 0 0 0 0 0 0" + \
               " --field vorticity_magnitude" + \
               " --colormaps rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r" + \
               " --norms none none none log log log" + \
@@ -157,8 +149,8 @@ if 9 in figures:
     os.system("python " + os.path.join(spath, "plot_slices_2x3.py") + \
               " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
               " --plane 'xy'" + \
-              " --loc 256" + \
-              " --zlabel '$z = \pi/2$'" + \
+              " --loc 128" + \
+              " --zlabel '$z = 0$'" + \
               " --steps 0 10 16 20 26 40" + \
               " --file_numbers 0 0 0 0 0 0" + \
               " --field vorticity_magnitude" + \
@@ -168,9 +160,35 @@ if 9 in figures:
               " --overwrite" + \
               " --save_path " + save_path)
 
-
 # Figure 10:
 if 10 in figures:
+    os.system("python " + os.path.join(spath, "plot_slices_2x3.py") + \
+              " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
+              " --plane 'xy'" + \
+              " --loc 256" + \
+              " --zlabel '$z = \pi/2$'" + \
+              " --steps 0 10 16 20 26 40" + \
+              " --file_numbers 0 0 0 0 0 0" + \
+              " --field vorticity_magnitude" + \
+              " --colormaps rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r" + \
+              " --norms none none none log log log" + \
+              " --fignum 10" + \
+              " --overwrite" + \
+              " --save_path " + save_path)
+
+# Figure 11 and 16:
+# create velocity, vorticity and helicity evolution:
+if 11 in figures or 16 in figures:
+    os.system("python " + os.path.join(spath, "plot_vor_vel_he_evolution.py") + \
+              " --filename " + os.path.join(fpath, "beltrami_256_fields.nc") + \
+              " --restartfile " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
+              " --fignum 11 16" + \
+              " --overwrite" + \
+              " --save_path " + save_path)
+
+
+# Figure 12:
+if 12 in figures:
     for i, step in enumerate([0, 10, 16, 20, 26, 40]):
         os.system("python " + os.path.join(spath, "plot_iso_surface.py") + \
                   " --filename " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
@@ -184,7 +202,7 @@ if 10 in figures:
                   " --opacity_vmin 1.0" + \
 	          " --opacity_points 0.0" +	\
 	          " --opacity_values 0.0" +	\
-                  " --fignum 10" + \
+                  " --fignum 12" + \
                   " --overwrite" + \
                   " --save_path " + save_path)
 
@@ -192,12 +210,12 @@ if 10 in figures:
               " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
               " --steps 0 10 16 20 26 40" + \
               " --file_numbers 0 0 0 0 0 0" + \
-              " --fignum 10" + \
+              " --fignum 12" + \
               " --overwrite" + \
               " --save_path " + save_path)
 
-# Figure 11:
-if 11 in figures:
+# Figure 13:
+if 13 in figures:
     os.system("python " + os.path.join(spath, "plot_slices_2x3.py") + \
               " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
               " --plane 'xy'" + \
@@ -208,78 +226,62 @@ if 11 in figures:
               " --field helicity" + \
               " --colormaps rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r rainbow4_r" + \
               " --norms centered centered centered centered centered centered" + \
-              " --fignum 11" + \
+              " --fignum 13" + \
               " --overwrite" + \
               " --save_path " + save_path)
 
-# Figure 12:
-if 12 in figures:
+# Figure 14:
+if 14 in figures:
+    os.system("python " + os.path.join(spath, "plot_contours.py") + \
+              " --filename " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
+              " --step 26"
+              " --fignum 14" + \
+              " --overwrite" + \
+              " --save_path " + save_path)
+
+# Figure 15:
+if 15 in figures:
     os.system("python " + os.path.join(spath, "plot_vor_vel_profiles.py") + \
               " --filenames " + os.path.join(fpath, "beltrami_256_restart_fields.nc ") + \
               os.path.join(fpath, "beltrami_256_fields.nc") + \
               " --steps 0 10 16 20 26 40 10" + \
               " --file_numbers 0 0 0 0 0 0 1" + \
-              " --fignum 12" + \
+              " --fignum 15" + \
               " --overwrite" + \
               " --save_path " + save_path)
 
-# Figure 13:
-# create velocity, vorticity and helicity evolution:
-if 13 in figures:
-    os.system("python " + os.path.join(spath, "plot_vor_vel_he_evolution.py") + \
-              " --filename " + os.path.join(fpath, "beltrami_256_fields.nc") + \
-              " --restartfile " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
-              " --fignum 110 13" + \
-              " --overwrite" + \
-              " --save_path " + save_path)
-
-# Figure 14:
+# Figure 17:
 # create power spectrum plot
-if 14 in figures:
+if 17 in figures:
     os.system("python " + os.path.join(spath, "plot_power_spectra.py") + \
               " --path " + fpath + \
               " --ncfile beltrami_256_fields.nc beltrami_256_restart_fields.nc" + \
               " --efile  beltrami_256_ecomp.asc" + \
-              " --fignum 14" + \
+              " --fignum 17" + \
               " --overwrite" + \
               " --save_path " + save_path)
 
-if 16 in figures:
+# Figure 18:
+if 18 in figures:
     os.system("python " + os.path.join(spath, "plot_vor2vel_conv.py") + \
               " --path " + os.path.join(fpath, "..", "tests") + \
-              " --fignum 16" + \
+              " --fignum 18" + \
               " --overwrite" + \
               " --save_path " + save_path)
 
-if 17 in figures:
+# Figure 19:
+if 19 in figures:
     os.system("python " + os.path.join(spath, "plot_filter_hyper.py") + \
               " --path " + os.path.join(fpath, "..", "tests") + \
               " --labels '$nz = 128$' '$nz = 256$'" + \
-              " --fignum 170" + \
+              " --fignum 19" + \
               " --overwrite" + \
               " --save_path " + save_path)
 
-
-if 150 in figures:
-    os.system("python " + os.path.join(spath, "plot_contours.py") + \
-              " --filename " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
-              " --step 26"
-              " --fignum 150" + \
-              " --overwrite" + \
-              " --save_path " + save_path)
-
-if 130 in figures:
+if 100 in figures:
     os.system("python " + os.path.join(spath, "plot_volume_fraction.py") + \
               " --filename " + os.path.join(fpath, "beltrami_256_fields.nc") + \
               " --restartfile " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
-              " --fignum 130" + \
-              " --overwrite" + \
-              " --save_path " + save_path)
-
-
-if 100 in figures:
-    os.system("python " + os.path.join(spath, "plot_diff_velocity_rms_evolution.py") + \
-              " --filename " + os.path.join(fpath, "beltrami_256_restart_fields.nc") + \
               " --fignum 100" + \
               " --overwrite" + \
               " --save_path " + save_path)
