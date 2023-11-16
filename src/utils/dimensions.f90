5c70214ffc8d8ba9244049766a0ca5e671f434c0
Matthias Frey <mf248@st-andrews.ac.uk>
2023-11-16 20:00:25 +0000

    add dimensions.f90

---
+++ src/utils/dimensions.f90
@@ -0,0 +1,9 @@
+module dimensions
+    implicit none
+
+    ! dimensional indices
+    integer, parameter :: I_X    = 1 & ! index for zonal direction
+                        , I_Y    = 2 & ! index for meridional direction
+                        , I_Z    = 3   ! index for vertical direction
+
+end module dimensions
