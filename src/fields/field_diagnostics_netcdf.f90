f0bbe6109e19c0dfec15f2449264804f49188ce6
Matthias Frey <mf248@st-andrews.ac.uk>
2024-08-01 09:28:02 +0100

    fix array access

--- src/fields/field_diagnostics_netcdf.f90
+++ src/fields/field_diagnostics_netcdf.f90
@@ -321,10 +321,11 @@ module field_diagnostics_netcdf
             buf(6) = bmin
 
             buf(7) = get_minimum_static_stability(l_global=.false.)
-#endif
 
             call mpi_blocking_reduce(buf(1:7), MPI_MIN, world)
-
+#else
+            call mpi_blocking_reduce(buf(1:4), MPI_MIN, world)
+#endif
 
             nc_dset(NC_OXMIN)%val = buf(1)
             nc_dset(NC_OYMIN)%val = buf(2)
