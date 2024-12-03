59554dc58c0a7a389f62e23be856401d01f08a30
Matthias Frey <mf248@st-andrews.ac.uk>
2024-12-03 17:23:56 +0000

    update test_implicit_rk4.f90

--- unit-tests/test_implicit_rk.f90
+++ unit-tests/test_implicit_rk.f90
@@ -23,14 +23,11 @@ program test_implicit_rk
     use mpi_layout
     use mpi_collectives, only : mpi_blocking_reduce
     use mpi_utils, only : mpi_stop
-    use model, only : layout
+    use model, only : layout, create_model
     implicit none
 
-    double precision :: error
-    integer          :: iz, nc, n
     double precision, allocatable :: ref(:, :, :, :), ed(:, :)
     double precision, allocatable :: src(:, :, :, :)
-    double precision :: time, time_step
 
     call mpi_env_initialise
 
@@ -49,193 +46,205 @@ program test_implicit_rk
 
     call mpi_layout_init(lower, extent, nx, ny, nz)
 
-    allocate(ref(0:nz, box%lo(2):box%hi(2), box%lo(1):box%hi(1), 3))
-    allocate(src(0:nz, box%lo(2):box%hi(2), box%lo(1):box%hi(1), 3))
-    allocate(ed(box%lo(2):box%hi(2), box%lo(1):box%hi(1)))
-
     call update_parameters
 
     call field_default
 
-!     call init_inversion
+    call run_test("uniform")
 
-    time_step = 0.1d0
-    do n = 1, 4
-        time = zero
-        svor = 2.0d0
-        ref = svor
-        src = one
-        svorts = src
-
-        vdiss = one
-
-        ed = dexp(-time_step * vdiss)
-        do nc = 1, 3
-            call layout%combine_semi_spectral(ref(:, :, :, nc))
-            call layout%combine_semi_spectral(src(:, :, :, nc))
-            do iz = 0, nz
-                ref(iz, :, :, nc) = ed * ref(iz, :, :, nc) + (one - ed) * src(iz, :, :, nc) / vdiss
-            enddo
-            call layout%decompose_semi_spectral(ref(:, :, :, nc))
-            call layout%decompose_semi_spectral(src(:, :, :, nc))
-        enddo
-
-        call impl_rk4(time, time_step)
-
-        error = maxval(dabs(ref - svor))
-
-        time_step = f12 * time_step
-
-    enddo
-
-    call mpi_blocking_reduce(error, MPI_MAX, world)
-
-    if (world%rank == world%root) then
-        call print_result_dp('Test implicit RK', error, atol=1.0e-12)
-    endif
-
-    deallocate(ref)
-    deallocate(src)
-    deallocate(ed)
+    call run_test("chebyshev")
 
     call mpi_env_finalise
 
 
-    contains
+contains
 
-        subroutine impl_rk4(t, dt)
-            double precision, intent(inout) :: t
-            double precision, intent(in)    :: dt
-            double precision                :: dt2, dt3, dt6
-            double precision                :: epq(box%lo(2):box%hi(2), &       ! exp(D * (t-t0))
-                                                   box%lo(1):box%hi(1))
-            double precision                :: emq(box%lo(2):box%hi(2), &       ! exp(- D * (t-t0))
-                                                   box%lo(1):box%hi(1))
-            double precision                :: qdi(0:nz, box%lo(2):box%hi(2), &
-                                                         box%lo(1):box%hi(1), 3)
-            double precision                :: qdf(0:nz, box%lo(2):box%hi(2), &
-                                                         box%lo(1):box%hi(1), 3)
+    subroutine run_test(grid_type)
+        character(*), intent(in)  :: grid_type
+        double precision          :: error
+        integer                   :: iz, nc, n
+        double precision          :: time, time_step
 
+        call create_model(grid_type, "Hou & Li")
 
-            dt2 = dt / 2.0d0
-            dt3 = dt / 3.0d0
-            dt6 = dt / 6.0d0
+        allocate(ref(0:nz, box%lo(2):box%hi(2), box%lo(1):box%hi(1), 3))
+        allocate(src(0:nz, box%lo(2):box%hi(2), box%lo(1):box%hi(1), 3))
+        allocate(ed(box%lo(2):box%hi(2), box%lo(1):box%hi(1)))
 
-            ! set integrating factors
-            emq = dexp(- dt2 * vdiss)
-            epq = 1.0d0 / emq
-
-            qdi = svor
-            svor = (qdi + dt2 * svorts)
-            do nc = 1, 3
-                call layout%combine_semi_spectral(svor(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
-                do iz = 0, nz
-                    svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
-                enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svor(:, :, :, nc))
-            enddo
-
-
-            qdf = qdi + dt6 * svorts
-
-            !------------------------------------------------------------------
-            !RK4 corrector step at time t0 + dt/2:
-            t = t + dt2
-
-            call vor2vel
-
-            call vorticity_tendency
-            svorts = src
-            ! apply integrating factors to source
-            do nc = 1, 3
-                call layout%combine_semi_spectral(svorts(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
-                do iz = 0, nz
-                    svorts(iz, :, :, nc) = svorts(iz, :, :, nc) * epq
-                enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svorts(:, :, :, nc))
-            enddo
-
-            svor = (qdi + dt2 * svorts)
-            do nc = 1, 3
-                call layout%combine_semi_spectral(svor(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
-                do iz = 0, nz
-                    svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
-                enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svor(:, :, :, nc))
-            enddo
-
-            qdf = qdf + dt3 * svorts
-
-            !------------------------------------------------------------------
-            !RK4 predictor step at time t0 + dt:
-
-            !Invert PV and compute velocity:
-            call vor2vel
-
-            call vorticity_tendency
-            svorts = src
-            ! apply integrating factors to source
-            do nc = 1, 3
-                call layout%combine_semi_spectral(svorts(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
-                do iz = 0, nz
-                    svorts(iz, :, :, nc) = svorts(iz, :, :, nc) * epq
-                enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svorts(:, :, :, nc))
-            enddo
-
-            emq = emq**2
-            svor = qdi + dt * svorts
-            do nc = 1, 3
-                call layout%combine_semi_spectral(svor(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
-                do iz = 0, nz
-                    svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
-                enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svor(:, :, :, nc))
-            enddo
-
-            qdf = qdf + dt3 * svorts
-
-            !------------------------------------------------------------------
-            !RK4 corrector step at time t0 + dt:
-            t = t + dt2
-
-            call vor2vel
-            call vorticity_tendency
+        time_step = 0.1d0
+        do n = 1, 4
+            time = zero
+            svor = 2.0d0
+            ref = svor
+            src = one
             svorts = src
 
-            epq = epq**2
-            ! apply integrating factors to source
+            vdiss = one
+
+            ed = exp(-time_step * vdiss)
             do nc = 1, 3
-                call layout%combine_semi_spectral(svorts(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
+                call layout%combine_semi_spectral(ref(:, :, :, nc))
+                call layout%combine_semi_spectral(src(:, :, :, nc))
                 do iz = 0, nz
-                    svorts(iz, :, :, nc) = svorts(iz, :, :, nc) * epq
+                    ref(iz, :, :, nc) = ed * ref(iz, :, :, nc) + (one - ed) * src(iz, :, :, nc) / vdiss
                 enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svorts(:, :, :, nc))
+                call layout%decompose_semi_spectral(ref(:, :, :, nc))
+                call layout%decompose_semi_spectral(src(:, :, :, nc))
             enddo
 
-            svor = qdf + dt6 * svorts
-            do nc = 1, 3
-                call layout%combine_semi_spectral(svor(:, :, :, nc))
-                !$omp parallel do private(iz)  default(shared)
-                do iz = 0, nz
-                    svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
-                enddo
-                !$omp end parallel do
-                call layout%decompose_semi_spectral(svor(:, :, :, nc))
-            enddo
+            call impl_rk4(time, time_step)
 
-        end subroutine impl_rk4
+            error = maxval(dabs(ref - svor))
+
+            time_step = f12 * time_step
+
+        enddo
+
+        call mpi_blocking_reduce(error, MPI_MAX, world)
+
+        if (world%rank == world%root) then
+            call print_result_dp('Test implicit RK ' // grid_type, error, atol=1.0e-12)
+        endif
+
+        deallocate(ref)
+        deallocate(src)
+        deallocate(ed)
+    end subroutine run_test
+
+    subroutine impl_rk4(t, dt)
+        double precision, intent(inout) :: t
+        double precision, intent(in)    :: dt
+        double precision                :: dt2, dt3, dt6
+        double precision                :: epq(box%lo(2):box%hi(2), &       ! exp(D * (t-t0))
+                                               box%lo(1):box%hi(1))
+        double precision                :: emq(box%lo(2):box%hi(2), &       ! exp(- D * (t-t0))
+                                               box%lo(1):box%hi(1))
+        double precision                :: qdi(0:nz, box%lo(2):box%hi(2), &
+                                                     box%lo(1):box%hi(1), 3)
+        double precision                :: qdf(0:nz, box%lo(2):box%hi(2), &
+                                                     box%lo(1):box%hi(1), 3)
+        integer                         :: iz, nc
+
+
+        dt2 = dt / 2.0d0
+        dt3 = dt / 3.0d0
+        dt6 = dt / 6.0d0
+
+        ! set integrating factors
+        emq = exp(- dt2 * vdiss)
+        epq = 1.0d0 / emq
+
+        qdi = svor
+        svor = (qdi + dt2 * svorts)
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svor(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svor(:, :, :, nc))
+        enddo
+
+
+        qdf = qdi + dt6 * svorts
+
+        !------------------------------------------------------------------
+        !RK4 corrector step at time t0 + dt/2:
+        t = t + dt2
+
+        call vor2vel
+
+        call vorticity_tendency
+        svorts = src
+        ! apply integrating factors to source
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svorts(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svorts(iz, :, :, nc) = svorts(iz, :, :, nc) * epq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svorts(:, :, :, nc))
+        enddo
+
+        svor = (qdi + dt2 * svorts)
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svor(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svor(:, :, :, nc))
+        enddo
+
+        qdf = qdf + dt3 * svorts
+
+        !------------------------------------------------------------------
+        !RK4 predictor step at time t0 + dt:
+
+        !Invert PV and compute velocity:
+        call vor2vel
+
+        call vorticity_tendency
+        svorts = src
+        ! apply integrating factors to source
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svorts(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svorts(iz, :, :, nc) = svorts(iz, :, :, nc) * epq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svorts(:, :, :, nc))
+        enddo
+
+        emq = emq**2
+        svor = qdi + dt * svorts
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svor(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svor(:, :, :, nc))
+        enddo
+
+        qdf = qdf + dt3 * svorts
+
+        !------------------------------------------------------------------
+        !RK4 corrector step at time t0 + dt:
+        t = t + dt2
+
+        call vor2vel
+        call vorticity_tendency
+        svorts = src
+
+        epq = epq**2
+        ! apply integrating factors to source
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svorts(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svorts(iz, :, :, nc) = svorts(iz, :, :, nc) * epq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svorts(:, :, :, nc))
+        enddo
+
+        svor = qdf + dt6 * svorts
+        do nc = 1, 3
+            call layout%combine_semi_spectral(svor(:, :, :, nc))
+            !$omp parallel do private(iz)  default(shared)
+            do iz = 0, nz
+                svor(iz, :, :, nc) = svor(iz, :, :, nc) * emq
+            enddo
+            !$omp end parallel do
+            call layout%decompose_semi_spectral(svor(:, :, :, nc))
+        enddo
+
+    end subroutine impl_rk4
 
 end program test_implicit_rk
