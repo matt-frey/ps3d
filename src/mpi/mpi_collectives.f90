5585d9f0fe4ad38e077db11571163f340305e441
Matthias Frey <mf248@st-andrews.ac.uk>
2025-01-14 09:41:53 +0000

    fix indentation of mpi modules

--- src/mpi/mpi_collectives.f90
+++ src/mpi/mpi_collectives.f90
@@ -7,34 +7,34 @@ module mpi_collectives
         module procedure :: mpi_integer_reduce
     end interface mpi_blocking_reduce
 
-    contains
+contains
 
-        subroutine mpi_double_reduce(sendbuf, op, comm)
-            double precision,   intent(inout) :: sendbuf(..)
-            type(MPI_Op),       intent(in)    :: op
-            type(communicator), intent(inout) :: comm
+    subroutine mpi_double_reduce(sendbuf, op, comm)
+        double precision,   intent(inout) :: sendbuf(..)
+        type(MPI_Op),       intent(in)    :: op
+        type(communicator), intent(inout) :: comm
 
-            if (comm%rank == comm%root) then
-                call MPI_Reduce(MPI_IN_PLACE, sendbuf, size(sendbuf), MPI_DOUBLE_PRECISION, &
-                                op, comm%root, comm%comm, comm%err)
-            else
-                call MPI_Reduce(sendbuf, sendbuf, size(sendbuf), MPI_DOUBLE_PRECISION, &
-                                op, comm%root, comm%comm, comm%err)
-            endif
-        end subroutine mpi_double_reduce
+        if (comm%rank == comm%root) then
+            call MPI_Reduce(MPI_IN_PLACE, sendbuf, size(sendbuf), MPI_DOUBLE_PRECISION, &
+                            op, comm%root, comm%comm, comm%err)
+        else
+            call MPI_Reduce(sendbuf, sendbuf, size(sendbuf), MPI_DOUBLE_PRECISION, &
+                            op, comm%root, comm%comm, comm%err)
+        endif
+    end subroutine mpi_double_reduce
 
-        subroutine mpi_integer_reduce(sendbuf, op, comm)
-            integer,            intent(inout) :: sendbuf(..)
-            type(MPI_Op),       intent(in)    :: op
-            type(communicator), intent(inout) :: comm
+    subroutine mpi_integer_reduce(sendbuf, op, comm)
+        integer,            intent(inout) :: sendbuf(..)
+        type(MPI_Op),       intent(in)    :: op
+        type(communicator), intent(inout) :: comm
 
-            if (comm%rank == comm%root) then
-                call MPI_Reduce(MPI_IN_PLACE, sendbuf, size(sendbuf), MPI_INTEGER, &
-                                op, comm%root, comm%comm, comm%err)
-            else
-                call MPI_Reduce(sendbuf, sendbuf, size(sendbuf), MPI_INTEGER, &
-                                op, comm%root, comm%comm, comm%err)
-            endif
-        end subroutine mpi_integer_reduce
+        if (comm%rank == comm%root) then
+            call MPI_Reduce(MPI_IN_PLACE, sendbuf, size(sendbuf), MPI_INTEGER, &
+                            op, comm%root, comm%comm, comm%err)
+        else
+            call MPI_Reduce(sendbuf, sendbuf, size(sendbuf), MPI_INTEGER, &
+                            op, comm%root, comm%comm, comm%err)
+        endif
+    end subroutine mpi_integer_reduce
 
 end module mpi_collectives
